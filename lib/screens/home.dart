a5eae8ce637d869ec39f176b44349f0d1b26e369
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2023-11-02 21:52:35 +0700

    custom appBar menjadi _buildAppBar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -6,9 +7,35 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My List')),
+      backgroundColor: tdBgColor,
+      appBar: _buildAppBar(),
       body: Container(
         child: Text('This is home screen'),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBgColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              child: Image.asset('assets/images/avatar.jpg'),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todo_list/screens/home.dart';
 
 void main() {
@@ -10,7 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ToDo App',
       home: Home(),
     );
