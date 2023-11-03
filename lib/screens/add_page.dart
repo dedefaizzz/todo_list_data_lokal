1bd74bab31c545e6b17aa71690ee488cb32a5508
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2023-11-03 22:49:42 +0700

    submit todo (form handling)

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/constants/colors.dart';
 
 class AddTodoPage extends StatelessWidget {
   const AddTodoPage({super.key});
@@ -11,11 +10,28 @@ class AddTodoPage extends StatelessWidget {
         title: Text('Add Todo'),
       ),
       body: ListView(
+        padding: EdgeInsets.all(20),
         children: [
-          TextField(),
-          TextField(),
+          TextField(
+            decoration: InputDecoration(hintText: 'Title'),
+          ),
+          SizedBox(height: 20),
+          TextField(
+            decoration: InputDecoration(hintText: 'Description'),
+            keyboardType: TextInputType.multiline,
+            minLines: 1,
+            maxLines: 10,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Submit'),
+          )
         ],
       ),
     );
   }
+
+  // Form handling
+  void submitData() {}
 }
